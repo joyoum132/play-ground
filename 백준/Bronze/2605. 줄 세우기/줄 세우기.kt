2c5ffa47c96ea0bbaaa297d4bda82876e383f0ee
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2022-08-08 00:31:43 +0900

    [Bronze II] Title: 줄 세우기, Time: 132 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.kt
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.kt
@@ -6,7 +6,14 @@ fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     for (i in 0 until size) {
         student[i] = nextInt()
     }
-    
+
+    /* NOTE
+    기존에 arrayList 방식처럼 res[0] = 1 처럼 사용할 경우 index out of bound 발생.
+    당연하지만 linkedList 를 자주 사용하지 않아서 실수하게 되었다.
+    linkedList 에서 특정 위치에 값을 insert 하는 경우, 현재의 순서를 유지하면서 특정 위체어 값을 insert 하려는 경우
+    .add 를 사용해야 한다.
+    index 를 통한 접근은 그 위치만을 update 시켜줄 뿐이다.
+    */
     val res = LinkedList<Int>()
     res.add(1)
 
