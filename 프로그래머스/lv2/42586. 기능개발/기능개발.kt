395661d8631e1ad72f60ef1a9ce7a15378538993
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2022-08-17 00:43:35 +0900

    [level 2] Title: 기능개발, Time: 5.89 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.kt
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.kt
@@ -1,31 +1,26 @@
+import java.util.*
+
 class Solution {
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
         val len = progresses.size
         if(len == 0) return intArrayOf(0)
         if(len == 1) return intArrayOf(1)
 
-        var count = 1
-        var date = 0
+        val q: Queue<Int> = LinkedList()
         val res = mutableListOf<Int>()
         progresses
             .mapIndexed { index, i ->
-            // 기능별 남은 작업일을 구한다.
-            if((100-i)%speeds[index] == 0 ) (100-i)/speeds[index]
-            else (100-i)/speeds[index]+1
-            }.forEachIndexed { index, i ->
-                if(index == 0) {
-                    date = i
-                } else {
-                    if(date < i) {
-                        res.add(count)
-                        count = 1
-                        date = i
-                    } else {
-                        count +=1
-                    }
-
-                    if(index == progresses.size-1) res.add(count)
+                // 기능별 남은 작업일을 구한다.
+                if((100-i)%speeds[index] == 0 ) (100-i)/speeds[index]
+                else (100-i)/speeds[index]+1
+            }
+            .forEachIndexed{ index, it ->
+                if(q.isNotEmpty() && q.peek() < it) {
+                    res.add(q.size)
+                    q.clear()
                 }
+                q.offer(it)
+                if(index == progresses.size-1) res.add(q.size)
             }
         return res.toIntArray()
     }
