103eb86a8f373d9009686c843de58d5ff0226520
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2022-12-27 00:49:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,22 +1,27 @@
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        var isZero = 0
-        var isMatched = 0
-        lottos.forEach {
-            if(it==0) isZero++
-            else if(win_nums.contains(it)) isMatched++
+       val numberOnlyLottos = lottos.filter { it != 0 }
+        val zeroCnt = lottos.size - numberOnlyLottos.size
+
+        var matched = 0
+
+        numberOnlyLottos
+            .forEach {
+                lotto -> if(win_nums.contains(lotto)) matched++
+            }
+
+        fun rank(matched:Int)
+        = when(matched) {
+            6 -> 1
+            5 -> 2
+            4 -> 3
+            3 -> 4
+            2 -> 5
+            else -> 6
         }
-        //최고 순위, 최저 순위
-        return intArrayOf(orders(isMatched+isZero), orders(isMatched))
-    }
-    
-    fun orders(cnt: Int) :Int =
-    when(cnt) {
-        6 -> 1
-        5 -> 2
-        4 -> 3
-        3 -> 4
-        2 -> 5
-        else -> 6
+        val min = rank(matched)
+        val max = rank(matched+zeroCnt)
+
+        return intArrayOf(max, min)
     }
 }
