34023c98204bbfa2cca688491800f591bc5fad42
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-02-11 17:25:28 +0900

    [level 1] Title: 모의고사, Time: 12.08 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -4,25 +4,25 @@ class Solution {
         val s2Seq = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
         val s3Seq = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
 
-        var s1 = Array(answers.size) { 0 }
-        var s2 = Array(answers.size) { 0 }
-        var s3 = Array(answers.size) { 0 }
-        //answers 만큼 수포자들의 intArray 를 새로 만들었는데, 그렇게 하지 않고 뱌로 정답 여부를 체크해서 count 하면 더 좋았을 것 같다.
-        for (i in answers.indices) {
-            s1[i] = s1Seq[i%(s1Seq.size)]
-            s2[i] = s2Seq[i%(s2Seq.size)]
-            s3[i] = s3Seq[i%(s3Seq.size)]
+        val isCorrect : MutableMap<Int, Int> = mutableMapOf(1 to 0, 2 to 0, 3 to 0)
+
+    for (i in answers.indices) {
+        val answer = answers[i]
+        if(s1Seq[i%(s1Seq.size)] == answer) {
+            isCorrect[1] = isCorrect[1]!! + 1
         }
+        if(s2Seq[i%(s2Seq.size)] == answer) {
+            isCorrect[2] = isCorrect[2]!! + 1
+        }
+        if(s3Seq[i%(s3Seq.size)] == answer) {
+            isCorrect[3] = isCorrect[3]!! + 1
+        }
+    }
 
-        val matched = intArrayOf(
-            s1.filterIndexed { index, i ->  answers[index] == i}.size,
-            s2.filterIndexed { index, i ->  answers[index] == i}.size,
-            s3.filterIndexed { index, i ->  answers[index] == i}.size
-        )
-        val maxCnt = matched.maxOf { it }
-        val answer = mutableListOf<Int>()
-        matched.forEachIndexed { index, i ->  if(i == maxCnt) {answer.add(index+1)} }
 
-        return answer.toIntArray()
+    val maxCnt = isCorrect.maxOf { it.value }
+    val answer = mutableListOf<Int>()
+    isCorrect.forEach { if(it.value == maxCnt) answer.add(it.key) }
+    return answer.toIntArray()
     }
 }
