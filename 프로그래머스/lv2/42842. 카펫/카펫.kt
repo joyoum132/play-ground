b48db7ecbbabde30e06253b773576364997abb63
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-09-12 17:16:27 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.kt
+++ 프로그래머스/lv2/42842. 카펫/카펫.kt
@@ -2,14 +2,13 @@ class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
         var answer = intArrayOf()
         if(yellow == 1) return intArrayOf(3, 3)
-        val end = yellow/2
-        (1..end).forEach {
-            val a = yellow / it
-            if(yellow % it == 0 && ((a+2)* (it+2) == brown+yellow)) {
-                answer  = intArrayOf(a+2, it+2)
-                return@forEach
+        for(i in 1..yellow/2) {
+            val a = yellow / i
+            if(yellow % i == 0 && ((a+2)* (i+2) == brown+yellow)) {
+                answer = intArrayOf(a+2, i+2)
+                break
             }
         }
-        return answer.sortedArrayDescending()
+        return answer
     }
 }
