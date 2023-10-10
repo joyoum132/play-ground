96bd9a4434a18cab3a19c249907d9fce37a123e9
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-10 20:00:35 +0900

    Time: 132 ms (93.49%), Space: 34.5 MB (63.09%) - LeetHub

--- leetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.kt
+++ leetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -10,7 +10,9 @@ class Solution {
             } else {
                 res.append(word1[i]).append(word2[i])
                 if(i == word1.length-1) {
-                    res.append(word2.slice(i+1..word2.length-1))
+                    repeat(word2.length-word1.length) {
+                        res.append(word2[i+it+1])
+                    }
                 }
             }
         }
