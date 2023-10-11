8f02c0240b162fbf4d70c8719bbf34f8784ee3ce
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-11 22:46:52 +0900

    Time: 366 ms (34.86%), Space: 42.6 MB (38.73%) - LeetHub

---
+++ leetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+        val s1 = nums1.toSet()
+        val s2 = nums2.toSet()
+
+        return listOf(s1.subtract(s2).toList(), s2.subtract(s1).toList())
+    }
+}
