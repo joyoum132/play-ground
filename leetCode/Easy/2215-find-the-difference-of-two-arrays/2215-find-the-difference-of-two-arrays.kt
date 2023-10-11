c891f4956227536b9535337c6ea974da1989a16e
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-11 23:00:51 +0900

    Time: 405 ms (7.75%), Space: 43.3 MB (8.80%) - LeetHub

--- leetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
+++ leetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -3,16 +3,16 @@ class Solution {
         // val s1 = nums1.toSet()
         // val s2 = nums2.toSet()
 
-        // return listOf(s1.subtract(s2).toList(), s2.subtract(s1).toList())
         
         val s1 = HashSet<Int>()
         val s2 = HashSet<Int>()
         nums1.forEach { s1.add(it) }
         nums2.forEach { s2.add(it) }
+        return listOf(s1.subtract(s2).toList(), s2.subtract(s1).toList())
 
-        return listOf(
-            s1.filterNot { s2.contains(it) },
-            s2.filterNot { s1.contains(it) }
-        )
+        // return listOf(
+            // s1.filterNot { s2.contains(it) },
+            // s2.filterNot { s1.contains(it) }
+        // )
     }
 }
