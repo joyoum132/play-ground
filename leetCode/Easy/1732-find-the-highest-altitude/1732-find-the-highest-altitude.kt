b4ebbd8b47c1b706750c6b7fbb2c6815544976bc
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-12 15:54:55 +0900

    Time: 144 ms (62.68%), Space: 33.9 MB (70.81%) - LeetHub

--- leetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
+++ leetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
@@ -1,13 +1,11 @@
 class Solution {
     fun largestAltitude(gain: IntArray): Int {
         var max = 0
-        var points = IntArray(gain.size+1){0}
+        var point = 0
         
         for(i in gain.indices) {
-            val v = points[i]+gain[i]
-            points[i+1] = v
-            
-            max = max(max, v)
+            point += gain[i]
+            max = max(max, point)
         }
         return max
     }
