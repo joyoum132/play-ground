38e2bb41f7b60fff4ac4471bd1b05d0ed2b3227b
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-16 22:09:05 +0900

    Time: 130 ms (66.18%), Space: 32.8 MB (77.94%) - LeetHub

--- leetCode/Medium/1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.kt
+++ leetCode/Medium/1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.kt
@@ -1,15 +1,23 @@
 class Solution {
     fun kthFactor(n: Int, k: Int): Int {
-        var i = 1
-        var factor = intArrayOf()
-        while(i*i < n) {
+        var cnt = 0
+        for(i in 1..n) {
             if(n%i == 0) {
-                factor += i
-                factor += n/i
+                cnt += 1
             }
-            i+=1
+            if(cnt == k) return i
         }
-        if(i*i == n) factor+=i
-        return factor.sorted().getOrElse(k-1) {-1}
+        return -1
+        // var i = 1
+        // var factor = intArrayOf()
+        // while(i*i < n) {
+        //     if(n%i == 0) {
+        //         factor += i
+        //         factor += n/i
+        //     }
+        //     i+=1
+        // }
+        // if(i*i == n) factor+=i
+        // return factor.sorted().getOrElse(k-1) {-1}
     }
 }
