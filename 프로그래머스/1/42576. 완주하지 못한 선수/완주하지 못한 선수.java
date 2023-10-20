09af69a709a08ef5ec4c190840e68b21bf0a51bd
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-20 12:15:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.52 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,38 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> pm = new HashMap<>();
-        Map<String, Integer> cm = new HashMap<>();
+        Map<String, Integer> m = new HashMap<>();
         for(String p : participant) {
-            pm.put(p, (pm.get(p) == null ? 0 : pm.get(p))  + 1);
+            m.put(p, m.getOrDefault(p, 0) + 1);
         }
 
         for(String c : completion) {
-            cm.put(c, (cm.get(c) == null ? 0 : cm.get(c)) + 1);
-
+            m.put(c, m.get(c)-1);
         }
 
-        for(String s : pm.keySet()) {
-            if(cm.get(s) ==  null) return s;
-            if(cm.get(s) < pm.get(s)) return s;
+        for (Map.Entry<String, Integer> etr : m.entrySet()) {
+            if(etr.getValue() > 0) return etr.getKey();
+            
         }
-
+        
         return "";
+        
+//         Map<String, Integer> pm = new HashMap<>();
+//         Map<String, Integer> cm = new HashMap<>();
+//         for(String p : participant) {
+//             pm.put(p, (pm.get(p) == null ? 0 : pm.get(p))  + 1);
+//         }
+
+//         for(String c : completion) {
+//             cm.put(c, (cm.get(c) == null ? 0 : cm.get(c)) + 1);
+
+//         }
+
+//         for(String s : pm.keySet()) {
+//             if(cm.get(s) ==  null) return s;
+//             if(cm.get(s) < pm.get(s)) return s;
+//         }
+
+//         return "";
     }
 }
