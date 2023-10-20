926ed7213d91bf243cc6e893b9f8cf3ae222d3e2
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-20 12:10:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.06 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> pm = new HashMap<>();
+        Map<String, Integer> cm = new HashMap<>();
+        for(String p : participant) {
+            pm.put(p, (pm.get(p) == null ? 0 : pm.get(p))  + 1);
+        }
+
+        for(String c : completion) {
+            cm.put(c, (cm.get(c) == null ? 0 : cm.get(c)) + 1);
+
+        }
+
+        for(String s : pm.keySet()) {
+            if(cm.get(s) ==  null) return s;
+            if(cm.get(s) < pm.get(s)) return s;
+        }
+
+        return "";
+    }
+}
