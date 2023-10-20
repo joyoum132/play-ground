dfbc24a4732ab1c2f6828820f0e1716b44a8c25d
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-20 11:34:39 +0900

    [level 1] Title: 폰켓몬, Time: 5.29 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> m = new HashMap<>();
-        for(int num : nums) {
-            if(m.containsKey(num)) {
-                m.put(num, m.get(num)+1);
-            } else {
-                m.put(num, 1);
-            }
-        }
-
-        return m.keySet().size() >= nums.length/2 ? nums.length/2 : m.keySet().size();
+        return Math.min(Arrays.stream(nums).distinct().toArray().length, nums.length/2);
     }
 }
