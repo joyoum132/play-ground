eb402e30c625ebd9fbeed7cdb0468603e4dd8c6d
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-10-26 22:33:33 +0900

    Time: 180 ms (5.20%), Space: 37.4 MB (12.13%) - LeetHub

--- leetCode/Medium/0198-house-robber/0198-house-robber.kt
+++ leetCode/Medium/0198-house-robber/0198-house-robber.kt
@@ -1,34 +1,13 @@
 class Solution {
     fun rob(nums: IntArray): Int {
         if(nums.size == 1) return nums[0]
-        var last = 0
         val robbed = IntArray(nums.size)
 
         robbed[0] = nums[0]
-        if(nums[0] < nums[1]) {
-            last = 1
-            robbed[1] = nums[1]
-        } else {
-            robbed[1] = nums[0]
-        }
+        robbed[1] = max(nums[0], nums[1])
 
         for(i in 2..nums.lastIndex) {
-            if(nums[i] == 0) {
-                robbed[i] = robbed[i-1]
-                continue
-            }
-
-            if(last == i-1) {
-                if(robbed[i-1] > robbed[i-2]+nums[i]) {
-                    robbed[i] = robbed[i-1]
-                } else {
-                    robbed[i] = robbed[i-2]+nums[i]
-                    last = i
-                }
-            } else {
-                robbed[i] = robbed[i-2] + nums[i]
-                last = i
-            }
+            robbed[i] = max(robbed[i-1], robbed[i-2]+nums[i])
         }
 
         return robbed[nums.size-1]
