bf9422f064f52be5e5c8c52b449cefd5a36de9ff
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-12-08 04:16:25 +0900

    Time: 1 ms (26.38%), Space: 44 MB (19.89%) - LeetHub

--- leetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ leetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -4,21 +4,17 @@ class Solution {
         int cnt=1;
         
         for(int i=1;i<nums.length;i++) {
-            if(nums[i] != nums[i-1]) {
-                cnt = 1;
-
-                //k위치에 값 넣어준다.        
-                nums[k] = nums[i];
-                k++;
-                
-            } else if(nums[i] == nums[i-1]) {
-                
-                if(cnt < 2) {
-                    //k위치에 값 넣어준다.
-                    nums[k] = nums[i];
-                    k++;
-                } 
+            
+            if(nums[i] == nums[i-1]) {
                 cnt++;
+            } else {
+                cnt = 1;
+            }
+            
+            
+            if(cnt <= 2) {
+                //k위치에 값 넣어준다.        
+                nums[k++] = nums[i];
             }
         }
         return k;
