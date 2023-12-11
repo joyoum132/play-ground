9c3b89455e1d826f4b4e5ebe5a25877b39af4f37
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2023-12-12 04:41:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 114.23 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 96.9 MB, 시간: 71.52 ms
+메모리: 95.9 MB, 시간: 114.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 12:14:58
+2023년 12월 2일 4:41:55
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,41 +1,22 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        /*solution 을 보고 수정한 코드
-        * hashmap은 하나로 합치고
-        * key, value가 둘 다 필요하기때문에 entrySet으로 루프*/
-        Map<String, Integer> m = new HashMap<>();
-        for(String p : participant) {
-            m.put(p, m.getOrDefault(p, 0) + 1);
-        }
+        HashMap<String, Integer> map = new HashMap<>();
 
-        for(String c : completion) {
-            m.put(c, m.get(c)-1);
+        for(String name : participant) {
+            map.compute(name, (k, val) -> val == null ? 1 : val+1);
         }
-
-        for (Map.Entry<String, Integer> etr : m.entrySet()) {
-            if(etr.getValue() > 0) return etr.getKey();
-            
+        for(String name: completion) {
+            int cnt = map.get(name)-1;
+            if(cnt == 0) map.remove(name);
+            else map.put(name, cnt);
+        }
+        String answer = "";
+        for (String s : map.keySet()) {
+            answer = s;
         }
         
-        return "";
-        
-//         Map<String, Integer> pm = new HashMap<>();
-//         Map<String, Integer> cm = new HashMap<>();
-//         for(String p : participant) {
-//             pm.put(p, (pm.get(p) == null ? 0 : pm.get(p))  + 1);
-//         }
-
-//         for(String c : completion) {
-//             cm.put(c, (cm.get(c) == null ? 0 : cm.get(c)) + 1);
-
-//         }
-
-//         for(String s : pm.keySet()) {
-//             if(cm.get(s) ==  null) return s;
-//             if(cm.get(s) < pm.get(s)) return s;
-//         }
-
-//         return "";
+        return answer;
     }
 }
