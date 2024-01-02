f6d1a81095ddf645ebbb0d174c175b9532c59105
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-01-03 00:11:06 +0900

    Time: 1 ms (78.89%), Space: 41.8 MB (20.41%) - LeetHub

--- leetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ leetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,50 +1,25 @@
 class Solution {
+    String[] words = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    List<String> answer = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        if(digits.isEmpty()) return new ArrayList<String>();
-        Map<Character, String> digitMap = new HashMap<>();
-        digitMap.put('2', "abc");
-        digitMap.put('3', "def");
-        digitMap.put('4', "ghi");
-        digitMap.put('5', "jkl");
-        digitMap.put('6', "mno");
-        digitMap.put('7', "pqrs");
-        digitMap.put('8', "tuv");
-        digitMap.put('9', "wxyz");
-
-        int digitSize = digits.length();
-        int[] range = new int[digitSize]; //위치별 반복해야하는 수
-        int[] curRange = new int[digitSize]; // 위치별 현재 반복한 수
-        int[] digitIdx = new int[digitSize]; // 위치별 입력해야하는 알파벳의 위치
-        String[] digit = new String[digitSize]; //위치별 입력 가능 알파벳
-
-        int output = 1; // answer 배열의 크기
-        for(int i=digitSize-1; i >= 0; i--) {
-            String alphabet = digitMap.get(digits.charAt(i));
-            output *= alphabet.length();
-            range[i] = output/alphabet.length();
-            curRange[i] = 1;
-            digitIdx[i] = 0;
-            digit[i] = alphabet;
-        }
-
-        List<String> answer = new ArrayList<>(output);
-        int cnt = 0;
-
-        while(cnt < output) {
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<digitSize; i++) {
-                sb.append(digit[i].charAt(digitIdx[i]));
-                if(curRange[i] >= range[i]) {
-                    digitIdx[i] = (digitIdx[i]+1)%digit[i].length();
-                    curRange[i] = 1;
-                } else {
-                    curRange[i]++;
-
-                }
-            }
-            answer.add(sb.toString());
-            cnt++;
-        }
+        if(digits.isEmpty()) return answer;
+        combine(digits, 0, new StringBuilder());
         return answer;
+        
+    }
+    
+    private void combine(String digits, int curIdx, StringBuilder letter) {
+        if(letter.length() == digits.length()) {
+            answer.add(letter.toString());
+            return;
+        }
+
+        String word = words[digits.charAt(curIdx)-'0'];
+        for(int i=0; i<word.length(); i++) {
+            letter.append(word.charAt(i));
+            combine(digits, curIdx+1, letter);
+            letter.deleteCharAt(letter.length()-1);
+        }
+
     }
 }
