00bbe93eefeda2d69cea8719ad7b4a5f69320a6f
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-01-06 11:59:40 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.10 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,31 +3,32 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-         //fees: 기본시간, 기본요금, 단위시간, 단위요금
+        //fees: 기본시간, 기본요금, 단위시간, 단위요금
         Map<String, List<Integer>> history = new HashMap<>();
 
         for(String record: records) {
             //record : 시각, 차량번호, 내역
             String[] recordArr = record.split(" ");
-            history.computeIfAbsent(recordArr[1],v -> new ArrayList<>())
-                    .add(toMinutes(recordArr[0])
-                    );
+            int assign = recordArr[2].equals("IN") ? -1 : 1;
+            history.computeIfAbsent(
+                            recordArr[1],
+                            v -> new ArrayList<>())
+                    .add(toMinutes(recordArr[0])*assign);
         }
+
         int[] answer = new int[history.keySet().size()];
         List<String> sortedKey = history.keySet().stream().sorted(Comparator.comparing(Integer::parseInt)).collect(Collectors.toList());
         int idx =0;
         for(String key: sortedKey) {
             List<Integer> inAndOut = history.get(key);
             if(inAndOut.size() % 2 == 1) inAndOut.add(1439);
-            int totalDuration = 0;
+            int totalDuration = inAndOut.stream().mapToInt(Integer::intValue).sum();
             int totalFee = fees[1];
-            for(int i=0;i<inAndOut.size();i+=2) {
-                totalDuration += inAndOut.get(i+1)-inAndOut.get(i);
-            }
 
             if(totalDuration > fees[0]) {
-                int out = (int)Math.ceil(((double)totalDuration-fees[0])/fees[2]);
-                totalFee += out*fees[3];
+                int exceedingTime = totalDuration-fees[0];
+                int additionalFee = (int) Math.ceil((double) exceedingTime / fees[2]) * fees[3];
+                totalFee += additionalFee;
             }
             answer[idx++] = totalFee;
         }
