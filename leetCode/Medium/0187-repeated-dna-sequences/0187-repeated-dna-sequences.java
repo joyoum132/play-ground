119a09a03c6c245da55e086e3ef653d89a05da73
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-01-06 15:23:50 +0900

    Time: 25 ms (28.76%), Space: 53 MB (26.47%) - LeetHub

--- leetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ leetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans = new ArrayList<>();
-        if(s.length() < 10) return ans;
-        
-        Map<String, Integer> dnaCnt = new HashMap<>();
+        Set<String> ans = new HashSet<>();
+        if(s.length()<10) return new ArrayList<>(ans);
+
         StringBuilder sb = new StringBuilder();
+        Set<String> danSeq = new HashSet<>();
 
         for(char c : s.toCharArray() ) {
             sb.append(c);
             if(sb.length() == 10) {
-                dnaCnt.compute(sb.toString(), (k, v) -> v == null ? 1 : v+1);
+                if(danSeq.contains(sb.toString())) {
+                    ans.add(sb.toString());
+                } else {
+                    danSeq.add(sb.toString());
+                }
                 sb.deleteCharAt(0);
             }
         }
-        
-        for(Map.Entry<String, Integer> entry : dnaCnt.entrySet()) {
-            if(entry.getValue() > 1) ans.add(entry.getKey());
-        }
-        return ans;
+
+        return new ArrayList<>(ans);
     }
 }
