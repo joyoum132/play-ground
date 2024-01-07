c82d6cd5b5e26e4d81916c352f87258cb5c192b8
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-01-07 15:17:10 +0900

    [level 2] Title: 타겟 넘버, Time: 0.37 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,19 @@
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        calc(target, 0, -1, numbers.length-1, numbers);
+        calc(target, 0, -1, numbers);
         return answer;
     }
     
-    void calc(int target, int total, int idx, int lastIdx, int[] numbers) {
-        if(idx == lastIdx) {
+    void calc(int target, int total, int idx, int[] numbers) {
+        if(idx == numbers.length-1) {
             if((total == target)) {
                 answer++;
             }
         } else {
             idx++;
-            calc(target, total+numbers[idx], idx, lastIdx, numbers);
-            calc(target, total-numbers[idx], idx, lastIdx, numbers);
+            calc(target, total+numbers[idx], idx, numbers);
+            calc(target, total-numbers[idx], idx, numbers);
         }
     }
 }
