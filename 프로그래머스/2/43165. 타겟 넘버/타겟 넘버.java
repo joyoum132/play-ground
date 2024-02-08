e904380b5961f46f98f268a83689954a68fb4428
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-02-09 00:40:27 +0900

    [level 2] Title: 타겟 넘버, Time: 0.30 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,19 @@
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        calc(target, 0, -1, numbers);
+        dfs(0,0, target, numbers);
         return answer;
     }
     
-    void calc(int target, int total, int idx, int[] numbers) {
+    private void dfs(int acc, int idx, int target, int[] numbers) {
+        //마지막 인덱스의 값이라면, 연산해서 target이 되는지 확인한다.
         if(idx == numbers.length-1) {
-            if((total == target)) {
-                answer++;
-            }
+            if(acc + numbers[idx] == target) answer++;
+            if(acc - numbers[idx] == target) answer++;
         } else {
-            idx++;
-            calc(target, total+numbers[idx], idx, numbers);
-            calc(target, total-numbers[idx], idx, numbers);
+            //acc는 어떻게 계산할 것인가?
+            dfs(acc+numbers[idx], idx+1, target, numbers);
+            dfs(acc-numbers[idx], idx+1, target, numbers);
         }
     }
 }
