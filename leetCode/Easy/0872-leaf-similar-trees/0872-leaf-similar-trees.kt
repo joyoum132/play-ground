e4e3313f34c8d3b26afe40af16886ef78233f56a
joyoum132 <98532450+joyoum132@users.noreply.github.com>
2024-03-17 01:01:35 +0900

    Time: 174 ms (13.59%), Space: 34.4 MB (55.98%) - LeetHub

--- leetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.kt
+++ leetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.kt
@@ -17,9 +17,9 @@ class Solution {
         rightStack.push(root2)
         
         /*
-        만약 leftSequence가 3,4,5,6 이고, rifhtSequence가 3,4,5 일때 while문의 조건이 ||가 되면
+        만약 leftSequence가 3,4,5,6 이고, rifhtSequence가 3,4,5 일때 while문의 조건이 &&가 되면
         rifhtStack.isNotEmpty() 조건이 먼저 끝나서 while문을 나오게되고 true를 리턴하게 된다,
-        그러나 &&가 된다면 rightVal은 -1, leftVal은 6이 되어 false를 리턴하게 된다.
+        그러나 ||가 된다면 rightVal은 -1, leftVal은 6이 되어 false를 리턴하게 된다.
         */
         while(leftStack.isNotEmpty() || rightStack.isNotEmpty()) {
             var leftVal = -1
@@ -29,13 +29,10 @@ class Solution {
                 val node = leftStack.pop()
                 if(node.left==null && node.right==null) {
                     leftVal = node.`val`
-                    println("leftTree's leaf is $leftVal")
                     break
                 } else {
-                    println("leftTree's branch is ${node.`val`}")
-                    node.right?.let{ leftStack.push(it) }
                     node.left?.let{ leftStack.push(it) }
-                    
+                    node.right?.let{ leftStack.push(it) }
                 }
             }
             
@@ -43,13 +40,10 @@ class Solution {
                 val node = rightStack.pop()
                 if(node.left==null && node.right==null) {
                     rightVal = node.`val`
-                    println("rightTree's leaf is $rightVal")
                     break
                 } else {
-                    println("rightTree's branch is ${node.`val`}")
-                    node.right?.let{ rightStack.push(it) }
                     node.left?.let{ rightStack.push(it) }
-                    
+                    node.right?.let{ rightStack.push(it) }
                 }
             }
             
